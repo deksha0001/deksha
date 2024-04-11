0ff88986063a02331b0e4c91f9d3b087435d7067
Deksha <159973271+deksha0001@users.noreply.github.com>
2024-04-10 20:14:57 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; 2024 Deksha| Student ID: 202106436</p>
+    <a href="admin/login.php" class="admin-login-button">Admin Panel Login</a>
+    <a href="editor/login.php" class="editor-login-button">editor Panel Login</a>
+</footer>
